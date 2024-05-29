1180fcc504d7cd7b10eacfd6890bd39bdaac5cca
andzheyevskiy <Igor_lifeking@hotmail.com>
2024-05-29 20:19:25 +0200

    Created main page

---
+++ assets/components/mainpage/mainpage.js
@@ -0,0 +1,10 @@
+$("#field").html(`
+<div class="mainWrapper">
+<a href="?search=films=1" class="mainContent"><img src="${$.baseURL}assets/img/films/0.jpg">films</a>
+<a href="?search=people=1" class="mainContent"><img src="${$.baseURL}/assets/img/people/0.jpg">people</a>
+<a href="?search=planets=1" class="mainContent"><img src="${$.baseURL}/assets/img/planets/0.jpg">planets</a>
+<a href="?search=species=1" class="mainContent"><img src="${$.baseURL}/assets/img/species/0.jpg">species</a>
+<a href="?search=vehicles=1" class="mainContent"><img src="${$.baseURL}/assets/img/vehicles/0.jpg">vehicles</a>
+<a href="?search=starships=1" class="mainContent"><img src="${$.baseURL}/assets/img/starships/0.jpg">starships</a>
+</div>
+`)
