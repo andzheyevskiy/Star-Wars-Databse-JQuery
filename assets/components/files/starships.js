95aaa2b5ab8b57d5ff31fa1aada97fdae4dede00
andzheyevskiy <Igor_lifeking@hotmail.com>
2024-05-29 20:19:04 +0200

    Created starhips page

---
+++ assets/components/files/starships.js
@@ -0,0 +1,45 @@
+// Depends on appendArray from $home/assets/moudles/appendArray.js
+
+const runFile = (id) => {
+    $.source = `${$.swapi}starships/`
+    $.finalUrl = $.source + id
+    $.ajax({
+        url: $.finalUrl,
+        success: function (e) {
+            $.img = `${$.baseURL}assets/img/starships/${e.name.replace(" ", "%20").replace("/", "")}.jpg`
+            // Add all elements in HTML
+            $("#field").html(`
+            <div class="file">
+            <h2>name</h2>
+            <p>${e.name}</p>
+            <img src="${$.img}">
+            <h2>model</h2>
+            <p>${e.model}</p>
+            <h2>starship class</h2>
+            <p>${e.starship_class}</p>
+            <h2>manufacturer</h2>
+            <p>${e.manufacturer}</p>
+            <h2>lenght</h2>
+            <p>${e.lenght}</p>
+            <h2>cost in credits</h2>
+            <p>${e.cost_in_credits}</p>
+            <h2>crew</h2>
+            <p>${e.crew}</p>
+            <h2>passengers</h2>
+            <p>${e.passengers}</p>
+            <h2>max atmospheric speed</h2>
+            <p>${e.max_atmospheric_speed}</p>
+            <h2>hyperdrive scale</h2>
+            <p>${e.hyperdrive_rating}</p>
+            <h2>cargo capacity</h2>
+            <p>${e.cargo_capacity}</p>
+            <h2>films</h2>
+            <p id="films"></p>${appendArray("films",e.films)}
+            </div>
+            `)
+        },
+        error: function () {
+            runError()
+        }
+    });
+}
